cc95acae089b34778366dc40a73ae58e0b3432be
liznomura <lizn@hawaii.edu>
2017-07-22 19:19:23 -1000

    done

--- indexing.sql
+++ indexing.sql
@@ -1,4 +1,6 @@
 \timing
+\timing
+
 DROP DATABASE indexed_cars;
 
 DROP USER indexed_cars_user;
@@ -7,7 +9,6 @@ CREATE USER indexed_cars_user;
 
 CREATE DATABASE indexed_cars WITH OWNER indexed_cars_user;
 
-DROP TABLE IF EXISTS car_models;
 
 \i scripts/car_models.sql;
 
@@ -22,29 +23,46 @@ DROP TABLE IF EXISTS car_models;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
 
+-- DROP INDEX IF EXISTS models;
+-- DROP INDEX IF EXISTS make_code_models;
+-- DROP INDEX IF EXISTS years;
+
+-- CREATE INDEX models
+-- ON car_models(make_code);
+
+-- CREATE INDEX make_code_models
+-- ON car_models(make_code, make_model);
+
+-- CREATE INDEX years
+-- ON car_models(year);
 
 
-SELECT DISTINCT make_title
-FROM car_models
-WHERE make_code = 'LAM';
---time: 28.121ms
+-- SELECT DISTINCT make_title
+-- FROM car_models
+-- WHERE make_code = 'LAM';
+-- --time: 28.121ms
+-- --new time: 0.958ms
 
-SELECT DISTINCT model_title
-FROM car_models
-WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
---time: 26.741ms
+-- SELECT DISTINCT model_title
+-- FROM car_models
+-- WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+-- --time: 26.741ms
+-- --new time: 1.458ms
 
-SELECT make_code, model_code, model_title, year
-FROM car_models
-WHERE make_code = 'LAM';
---time: 30.833ms
+-- SELECT make_code, model_code, model_title, year
+-- FROM car_models
+-- WHERE make_code = 'LAM';
+-- --time: 30.833ms
+-- --new time: 1.589ms
 
-SELECT *
-FROM car_models
-WHERE year >= 2010 AND year <= 2015;
---time: 91.612ms
+-- SELECT *
+-- FROM car_models
+-- WHERE year >= 2010 AND year <= 2015;
+-- --time: 91.612ms
+-- --new time: 72.807ms
 
-SELECT *
-FROM car_models
-WHERE year = 2010;
---time: 46.613ms
+-- SELECT *
+-- FROM car_models
+-- WHERE year = 2010;
+-- --time: 46.613ms
+-- --new time: 17.087ms
